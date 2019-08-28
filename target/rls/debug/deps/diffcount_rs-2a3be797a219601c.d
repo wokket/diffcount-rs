aa4c125fe5f764e574d4aefac7a576260fe7f8ae
wokket <wokket@gmail.com>
2019-08-28 15:59:43 +1000

    Removing files that should have been ignored

--- target/rls/debug/deps/diffcount_rs-2a3be797a219601c.d
+++
@@ -1,5 +0,0 @@
-d:\dev\diffcount-rs\target\rls\debug\deps\diffcount_rs-2a3be797a219601c.rmeta: src\main.rs
-
-d:\dev\diffcount-rs\target\rls\debug\deps\diffcount_rs-2a3be797a219601c.d: src\main.rs
-
-src\main.rs:
