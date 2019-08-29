bb7bf111f4c23fe04c7389ca3430ac3993c5819a
wokket <wokket@gmail.com>
2019-08-29 16:50:46 +1000

    feat: Basic publish script to make distributable

---
+++ build_release.bat
@@ -0,0 +1,18 @@
+@echo off
+
+cargo build --release
+
+if exist .\publish\ rmdir /S /Q .\publish
+
+mkdir publish
+
+copy target\release\*.exe .\publish\
+copy %VCPKGDIR%\installed\x64-windows\bin\*.dll .\publish\
+
+:: Get The icons copied
+mkdir publish\share\icons
+
+:: TODO: Only copy the icons we're actually using....
+:: Remming out until then as it's waaay too many files just for minimise icons...
+::xcopy %VCPKGDIR%\installed\x64-windows\share\icons publish\share\icons /S
+
--- readme.md
+++ readme.md
@@ -1,3 +1,6 @@
 # DiffCell#
 
 Cell Counting 101 - playing with GTK in rust.
+
+
+Requires the VC++ Runtime to execute: https://www.microsoft.com/en-us/download/confirmation.aspx?id=52685
